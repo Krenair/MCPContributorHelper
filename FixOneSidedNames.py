9d3a8764d8266eaf91889e5877bff1e2c479406a
Krenair <krenair@gmail.com>
2012-06-21 22:43:40 +0100

    Oops, remove a debug line.

--- FixOneSidedNames.py
+++ FixOneSidedNames.py
@@ -3,7 +3,6 @@ def checkNames (clientinfo, serverinfo):
     if not ContributorTools.isSeargeName(clientinfo['name']) and ContributorTools.isSeargeName(serverinfo['name']):
         safeOutput('0', serverinfo['name'], 'pc' + serverinfo['searge'][1].replace('u', 'm').replace('i', 'f') + ' ' + clientinfo['searge'] + ' ' + serverinfo['searge'])
     elif ContributorTools.isSeargeName(clientinfo['name']) and not ContributorTools.isSeargeName(serverinfo['name']):
-        print clientinfo['name'], serverinfo['name']
         safeOutput('1', clientinfo['name'], 'ps' + clientinfo['searge'][1].replace('u', 'm').replace('i', 'f') + ' ' + serverinfo['searge'] + ' ' + clientinfo['searge'])
 
 def safeOutput(side, name, out):
